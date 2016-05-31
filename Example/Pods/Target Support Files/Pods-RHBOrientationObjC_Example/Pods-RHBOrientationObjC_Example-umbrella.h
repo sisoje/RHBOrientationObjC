8359ad1dac29394cfae1f951612a9737a15557b7
Lazar Otasevic <lazar.otasevic@1und1.de>
2016-05-31 17:31:58 +0200

    pod example

---
+++ Example/Pods/Target Support Files/Pods-RHBOrientationObjC_Example/Pods-RHBOrientationObjC_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RHBOrientationObjC_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RHBOrientationObjC_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/RHBOrientationObjC/RHBOrientationObjC-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double RHBOrientationObjCVersionNumber;
+FOUNDATION_EXPORT const unsigned char RHBOrientationObjCVersionString[];
+
